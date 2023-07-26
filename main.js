a6358e560fa88ccc6b449530080c6a3bfdf87131
azlanajju <muhammedazlan11@gmail.com>
2023-07-26 19:09:55 +0530

    added everything by azlan

--- main.js
+++ main.js
@@ -1,61 +1,59 @@
-const numAttendeesInput = document.getElementById('numAttendees');
 const vegCountInput = document.getElementById('vegCount');
 const nonVegCountInput = document.getElementById('nonVegCount');
+const vegCountField = document.getElementById('vegCoutField');
+const nonVegCountField = document.getElementById('nonVegCoutField');
 
 function incrVeg() {
+  document.getElementById('nonVegBtn').removeAttribute('disabled');
+  var counterVeg = document.getElementById('vegCount');
+  var vegBtn = document.getElementById('vegBtn');
+  counterVeg.value++;
+  if (parseInt(counterVeg.value) >= 4) {
+    vegBtn.setAttribute('disabled', 'true');
+  }
+  updateNonVegOptions();
 
-
-    document.getElementById('nonVegBtn').removeAttribute('disabled');
-    var counterVeg = document.getElementById('vegCount');
-
-    var counterNonVeg = document.getElementById('nonVegCount');
-
-    var vegBtn = document.getElementById('vegBtn');
-    counterVeg.value++;
-    if (parseInt(counterVeg.value) >= parseInt(numAttendeesInput.value)) {
-        vegBtn.setAttribute('disabled', 'true');
-
-
-    }
-    updateNonVegOptions();
-
-
-
-
+  // Display the updated value in vegCoutField
+  vegCountField.innerText = counterVeg.value;
 }
+
 function incrNonVeg() {
+  document.getElementById('vegBtn').removeAttribute('disabled');
+  var counterVeg = document.getElementById('nonVegCount');
+  var vegBtn = document.getElementById('nonVegBtn');
+  counterVeg.value++;
+  if (parseInt(counterVeg.value) >= 4) {
+    vegBtn.setAttribute('disabled', 'true');
+  } else { // Here's the correction
+    vegBtn.removeAttribute('disabled');
+  }
+  updateVegOptions();
 
-    document.getElementById('vegBtn').removeAttribute('disabled');
-
-
-    var counterVeg = document.getElementById('nonVegCount');
-    var vegBtn = document.getElementById('nonVegBtn');
-    counterVeg.value++;
-    if (parseInt(counterVeg.value) >= parseInt(numAttendeesInput.value)) {
-        vegBtn.setAttribute('disabled', 'true');
-    } else { // Here's the correction
-        vegBtn.removeAttribute('disabled');
-    }
-    updateVegOptions();
+  // Display the updated value in nonVegCoutField
+  nonVegCountField.innerText = counterVeg.value;
 }
-
 
 vegCountInput.addEventListener('change', updateNonVegOptions);
 nonVegCountInput.addEventListener('change', updateVegOptions);
 
 function updateNonVegOptions() {
-    const maxNonVegAllowed = parseInt(numAttendeesInput.value) - parseInt(vegCountInput.value);
-    nonVegCountInput.max = maxNonVegAllowed;
-    if (parseInt(nonVegCountInput.value) > maxNonVegAllowed) {
-        nonVegCountInput.value = maxNonVegAllowed;
-    }
+  const maxNonVegAllowed = 4 - parseInt(vegCountInput.value);
+  nonVegCountInput.max = maxNonVegAllowed;
+  if (parseInt(nonVegCountInput.value) > maxNonVegAllowed) {
+    nonVegCountInput.value = maxNonVegAllowed;
+  }
+
+  // Display the updated value in nonVegCoutField
+  nonVegCountField.innerText = nonVegCountInput.value;
 }
 
 function updateVegOptions() {
-    const maxVegAllowed = parseInt(numAttendeesInput.value) - parseInt(nonVegCountInput.value);
-    vegCountInput.max = maxVegAllowed;
-    if (parseInt(vegCountInput.value) > maxVegAllowed) {
-        vegCountInput.value = maxVegAllowed;
-    }
-}
+  const maxVegAllowed = 4 - parseInt(nonVegCountInput.value);
+  vegCountInput.max = maxVegAllowed;
+  if (parseInt(vegCountInput.value) > maxVegAllowed) {
+    vegCountInput.value = maxVegAllowed;
+  }
 
+  // Display the updated value in vegCoutField
+  vegCountField.innerText = vegCountInput.value;
+}
